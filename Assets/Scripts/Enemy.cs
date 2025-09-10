edd1b94f7f470bffa78ecffa331ab5b23d7b6bc3
Mounir Ziad <mo697861@ucf.edu>
2025-09-10 15:36:28 -0400

    Yuh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public Transform player;
-    public float baseSpeed = 2;
+    public float baseSpeed = 2f;
+    public float orbitFactor = 0.7f; // how much is sideways vs inward pull
     public float avoidanceStrength = 2f;
     public float avoidanceRadius = 1f;
     public float speed;
@@ -15,36 +16,43 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
-        Vector2 toPlayer = (player.position - transform.position).normalized;
+        Vector2 toPlayer = (player.position - transform.position);
         distance = toPlayer.magnitude;
-
         Vector2 toPlayerNormalized = toPlayer.normalized;
-        Vector2 orbitDir = new Vector2(-toPlayer.y, toPlayer.x).normalized;
-        speed = baseSpeed + distance * 0.5f;
 
-        Collider2D hit = Physics2D.OverlapCircle(transform.position, avoidanceRadius);
-    
-        if (hit != null && hit.gameObject != gameObject)
+        // Orbit direction (perpendicular to player vector)
+        Vector2 orbitDir = new Vector2(-toPlayerNormalized.y, toPlayerNormalized.x);
+
+        // Mix orbit and pull so enemy doesn't drift away
+        Vector2 moveDir = (toPlayerNormalized * (1f - orbitFactor)) + (orbitDir * orbitFactor);
+
+        // Speed scales a little with distance
+        speed = baseSpeed + distance * 0.3f;
+
+        // Avoidance from other enemies
+        Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, avoidanceRadius);
+        foreach (Collider2D h in hits)
         {
-            Vector2 awayvariable = (transform.position - hit.transform.position).normalized;
-            orbitDir += awayvariable * avoidanceStrength;
+            if (h != null && h.gameObject != gameObject)
+            {
+                Vector2 away = (transform.position - h.transform.position).normalized;
+                moveDir += away * avoidanceStrength;
+            }
         }
 
-        orbitDir.Normalize();
+        moveDir.Normalize();
 
+        // Move enemy
+        Vector2 newPos = rb.position + moveDir * speed * Time.fixedDeltaTime;
+        rb.MovePosition(newPos);
 
-        Vector2 velocity = orbitDir * speed * Time.deltaTime;
-        rb.MovePosition(velocity + rb.position);
-
-        float angleToPlayer = Mathf.Atan2(toPlayer.y, toPlayer.x) * Mathf.Rad2Deg - 90f;
-
-        transform.rotation = Quaternion.Euler(0, 0, angleToPlayer);
-        //transform.rotation = Quaternion.Lerp(transform.rotation, targetrot, 5 * Time.deltaTime);
-
+        // Rotate to face movement direction
+        float angle = Mathf.Atan2(moveDir.y, moveDir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
     }
 }
