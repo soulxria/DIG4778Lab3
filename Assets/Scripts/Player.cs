cbf7b6c1e035fc0d000726bf690c8cf0f83e92fd
Dylan/Nyx <163457812+soulxria@users.noreply.github.com>
2025-09-10 16:56:19 -0400

    playerMovement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 3.0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        float move = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float moveV = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(move, 0, 0);
+        transform.Translate(0, moveV, 0);
     }
 }
